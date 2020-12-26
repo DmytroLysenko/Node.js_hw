32e935ab171c92ea8537da34c1958d58cd61b8e9
DmytroLysenko <DmytroLysenko@users.noreply.github.com>
2020-12-26 11:42:12 +0200

    fixed: contacts validation

--- contacts/contacts.validations.js
+++ contacts/contacts.validations.js
@@ -1,43 +1,42 @@
 const Joi = require("joi");
 
+const nameSchema = Joi.string().alphanum().min(3).max(30);
+const emailSchema = Joi.string().email({
+  minDomainSegments: 2,
+  tlds: { allow: true },
+});
+const phoneSchema = Joi.string().pattern(
+  /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/
+);
+
 const validateAddContact = (req, res, next) => {
-  const schema = Joi.object().keys({
-    name: Joi.string().alphanum().min(3).max(30).required(),
-    email: Joi.string()
-      .email({
-        minDomainSegments: 2,
-        tlds: { allow: true },
-      })
-      .required(),
-    phone: Joi.string()
-      .pattern(/^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/)
-      .required(),
+  const schema = Joi.object({
+    name: nameSchema.required(),
+    email: emailSchema.required(),
+    phone: phoneSchema.required(),
   });
 
-  const validationError = schema.validate(req.body).error;
+  const validationResult = schema.validate(req.body, { abortEarly: false });
 
-  if (validationError) {
-    return res.status(400).json({ message: validationError });
+  if (validationResult.error) {
+    return res.status(400).send({ message: validationResult.error.message });
   }
 
   next();
 };
 
 const validateEditContact = (req, res, next) => {
-  const schema = Joi.object().keys({
-    name: Joi.string().alphanum().min(3).max(30),
-    email: Joi.string().email({
-      minDomainSegments: 2,
-      tlds: { allow: true },
-    }),
-    phone: Joi.string().pattern(/^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/),
+  const schema = Joi.object({
+    name: nameSchema,
+    email: emailSchema,
+    phone: phoneSchema,
   });
 
-  const validationError = schema.validate(req.body).error;
+  const validationResult = schema.validate(req.body, { abortEarly: false });
   const id = parseInt(req.params.id);
 
-  if (validationError) {
-    return res.status(400).json({ message: validationError });
+  if (validationResult.error) {
+    return res.status(400).json({ message: validationResult.error.message });
   }
 
   if (!id) {
